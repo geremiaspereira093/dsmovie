11f1870f7b87a4762dcee587d85dcc77fc894024
geremiaspereira093 <geresilva093@gmail.com>
2022-03-14 00:31:46 -0300

    arrumar deploy

--- frontend/src/componentes/MovieStars/index.tsx
+++ frontend/src/componentes/MovieStars/index.tsx
@@ -32,10 +32,10 @@ function getFills(score: number) {
     return fills;
   }
 function Stars({fill}: StarProps){
-    if( fill == 0){
+    if( fill === 0){
        return <StarEmpty/>  
     }
-    else if(fill == 1){
+    else if(fill === 1){
         return <StarFull/>
     }
     else{
